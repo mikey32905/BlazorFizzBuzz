206405bb64663a86a56ee1ddc39840166b023f00
Mike Williams <mikey32905@gmail.com>
2025-05-05 16:46:56 -0400

    Add project files.

---
+++ Models/FizzBuzzModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorFizzBuzz.Models
+{
+    public class FizzBuzzModel
+    {
+        [Required(ErrorMessage = "Please enter a Fizz Value.")]
+        [Range(1, 100, ErrorMessage = "Fizz Value must be between 1 and 100.")]
+        public int FizzValue { get; set; } = 3;
+
+        [Required(ErrorMessage = "Please enter a Buzz Value.")]
+        [Range(1, 100, ErrorMessage = "Buzz Value must be between 1 and 100.")]
+        public int BuzzValue { get; set; } = 5;
+
+        [Required(ErrorMessage = "Please enter a Stop Value.")]
+        [Range(1, 1000, ErrorMessage = "Stop Value must be between 1 and 1000.")]
+        public int StopValue { get; set; } = 100;
+
+    }
+}
